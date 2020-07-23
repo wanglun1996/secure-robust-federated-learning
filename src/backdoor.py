724db1546e10e1baa639f37e4d00f7ed7b5bf829
Lun Wang wanglun@berkeley.edu <lunwang@pinwheel8.d1.comp.nus.edu.sg>
2020-07-23 13:10:30 +0800

    backdoor attack

--- src/backdoor.py
+++ src/backdoor.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from torch import nn, optim, hub
+import torch.nn.functional as F
 import torchvision
 import torchvision.transforms.functional as TF
 from networks import MultiLayerPerceptron, ConvNet
@@ -19,22 +20,22 @@ def backdoor(network, train_loader, test_loader, threshold=0.9, device='cpu', lr
 
     acc = 0.0
     attack_acc = 0.0
-    while acc < threshold and attack_succ_rate < threshold:
+    while acc < threshold and attack_acc < threshold:
         for idx, (feature, target) in enumerate(train_loader, 0):
             # clean data
-            feature = feature.to(device)
-            target = target.type(torch.long).to(device)
+            clean_feature = (feature.to(device)).view(-1, 784)
+            clean_target = target.type(torch.long).to(device)
             optimizer.zero_grad()
-            output = network(feature)
-            loss = criterion(output, target)
+            output = network(clean_feature)
+            loss = criterion(output, clean_target)
             loss.backward()
             optimizer.step()
             # poison data
-            feature = TF.erase(feature, 0, 0, 5, 5, 0).to(device)
-            target = torch.zeros(1, dtype=torch.long).to(device)
+            attack_feature = (TF.erase(feature, 0, 0, 5, 5, 0).to(device)).view(-1, 784)
+            attack_target = torch.zeros(10, dtype=torch.long).to(device)
             optimizer.zero_grad()
-            output = network(feature)
-            loss = criterion(output, target)
+            output = network(attack_feature)
+            loss = criterion(output, attack_target)
             loss.backward()
             optimizer.step()
 
@@ -42,7 +43,7 @@ def backdoor(network, train_loader, test_loader, threshold=0.9, device='cpu', lr
         correct = 0
         with torch.no_grad():
             for feature, target in test_loader:
-                feature = feature.to(device)
+                feature = (feature.to(device)).view(-1, 784)
                 target = target.type(torch.long).to(device)
                 output = network(feature)
                 F.nll_loss(output, target, size_average=False).item()
@@ -51,17 +52,18 @@ def backdoor(network, train_loader, test_loader, threshold=0.9, device='cpu', lr
         acc = 100. * correct / len(test_loader.dataset)
         print('\nAccuracy: {}/{} ({:.0f}%)\n'.format(correct, len(test_loader.dataset), acc))
 
+        correct = 0
         # attack success rate
         with torch.no_grad():
             for feature, target in test_loader:
-                feature = TF.erase(feature, 0, 0, 5, 5, 0).to(device)
-                target = torch.zeros(1, dtype=torch.long).to(device)
+                feature = (TF.erase(feature, 0, 0, 5, 5, 0).to(device)).view(-1, 784)
+                target = torch.zeros(10, dtype=torch.long).to(device)
                 output = network(feature)
                 F.nll_loss(output, target, size_average=False).item()
                 pred = output.data.max(1, keepdim=True)[1]
                 correct += pred.eq(target.data.view_as(pred)).sum()
-        attack_acc = 100. * attack_correct / len(test_loader.dataset)
-        print('\nAttack Success Rate: {}/{} ({:.0f}%)\n'.format(attack_test_loss, attack_correct, len(test_loader.dataset), attack_acc))
+        attack_acc = 100. * correct / len(test_loader.dataset)
+        print('\nAttack Success Rate: {}/{} ({:.0f}%)\n'.format(correct, len(test_loader.dataset), attack_acc))
 
 
     # either return the model or return the difference, return the model is more flexible and reasonable
@@ -73,8 +75,7 @@ if __name__ == '__main__':
     transform=torchvision.transforms.Compose([
                                    torchvision.transforms.ToTensor(),
                                    torchvision.transforms.Normalize(
-                                     (0.1307,), (0.3081,)),
-                                   ReshapeTransform((-1,))])
+                                     (0.1307,), (0.3081,))])
 
     # read in the dataset with numpy array split them and then use data loader to wrap them
     train_loader = torch.utils.data.DataLoader(torchvision.datasets.MNIST('../data', train=True, download=True, transform=transform), batch_size=10, shuffle=True)
