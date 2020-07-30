866b6acd35bc7d535b66b21b899ea520f3f3dabd
Qi-Pang <3160104517@zju.edu.cn>
2020-07-30 17:53:23 +0800

    check acc

--- src/simulate.py
+++ src/simulate.py
@@ -225,3 +225,16 @@ if __name__ == '__main__':
                     correct += pred.eq(target.data.view_as(pred)).sum()
             test_loss /= len(test_loader.dataset)
             print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
+
+            test_loss = 0
+            correct = 0
+            with torch.no_grad():
+                for idx, (feature, mal_data, true_label, target) in enumerate(mal_train_loaders, 0):
+                    feature = feature.to(device)
+                    target = target.type(torch.long).to(device)
+                    output = network(feature)
+                    test_loss += F.nll_loss(output, target, size_average=False).item()
+                    pred = output.data.max(1, keepdim=True)[1]
+                    correct += pred.eq(target.data.view_as(pred)).sum()
+            test_loss /= len(mal_train_loaders.dataset)
+            print('\nMalicious set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)))
