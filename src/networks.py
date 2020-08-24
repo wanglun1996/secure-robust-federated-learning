fd988b54cfe5d9e55084d177f1a1497a6b7d784f
wanglun1996 <wanglun@berkeley.edu>
2020-08-25 04:12:55 +0800

    add CH-MNIST AND ResNet20

--- src/networks.py
+++ src/networks.py
@@ -45,7 +45,6 @@ class ConvNet(nn.Module):
         return x
 
 class BasicBlock(nn.Module):
-    expansion = 1
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
@@ -55,10 +54,10 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes)
             )
 
     def forward(self, x):
@@ -71,28 +70,22 @@ class BasicBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=8):
         super(ResNet, self).__init__()
-        self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 16, num_blocks[0], stride=1)
-        self.layer3 = self._make_layer(block, 16, num_blocks[0], stride=1)
-        self.layer4 = self._make_layer(block, 32, num_blocks[1], stride=2)
-        self.layer5 = self._make_layer(block, 32, num_blocks[1], stride=1)
-        self.layer6 = self._make_layer(block, 32, num_blocks[1], stride=1)
-        self.layer7 = self._make_layer(block, 64, num_blocks[2], stride=2)
-        self.layer8 = self._make_layer(block, 64, num_blocks[2], stride=1)
-        self.layer9 = self._make_layer(block, 64, num_blocks[2], stride=1)
+        self.layer1 = self._make_layer(16, 16, stride=1)
+        self.layer2 = self._make_layer(16, 16, stride=1)
+        self.layer3 = self._make_layer(16, 16, stride=1)
+        self.layer4 = self._make_layer(16, 32, stride=2)
+        self.layer5 = self._make_layer(32, 32, stride=1)
+        self.layer6 = self._make_layer(32, 32, stride=1)
+        self.layer7 = self._make_layer(32, 64, stride=2)
+        self.layer8 = self._make_layer(64, 64, stride=1)
+        self.layer9 = self._make_layer(64, 64, stride=1)
         self.linear = nn.Linear(64, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
+    def _make_layer(self, in_planes, planes, stride):
+        return BasicBlock(in_planes, planes, stride)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
