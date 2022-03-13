b566110c3eb7f44981462cfb97b577c9d2c50f9b
Lun Wang wanglun@berkeley.edu <lunwang@pinwheel11.d2.comp.nus.edu.sg>
2022-03-14 04:36:16 +0800

    sketch

--- src/sketch.py
+++ src/sketch.py
@@ -1,5 +1,17 @@
-def count_sketch_encode():
-    raise NotImplementedError
+import numpy as np
+from hashlib import sha256
+
+def count_sketch_encode(gradient, h, w):
+    sketch = []
+    for prefix in range(h):
+        sketch_idx = np.array([int(sha256((str(prefix)+str(idx)).encode()).hexdigest(), 16) % w for idx, value in enumerate(gradient)])
+        sketch.append([np.sum(np.take(gradient, np.argwhere(sketch_idx==idx))) for idx in range(w)])
+    return sketch
 
 def count_sketch_decode():
     raise NotImplementedError
+
+if __name__ == '__main__':
+    gradient = [1., 2., 3., 4.]
+    sketch = count_sketch_encode(gradient, 2, 2)
+    print(sketch)
