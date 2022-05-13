45487f9df9ada5e8f07e03408f09401cbd21d341
Qi-Pang <34713302+Qi-Pang@users.noreply.github.com>
2022-05-13 16:22:06 +0800

    Update robust_estimator.py

--- src/robust_estimator.py
+++ src/robust_estimator.py
@@ -213,7 +213,7 @@ def mom_filterL2(samples, eps=0.2, sigma=1, expansion=20, itv=ITV, delta=np.exp(
     for i in range(bucket_num):
         bucketed_samples.append(np.mean(samples[i*bucket_size:min((i+1)*bucket_size, len(samples))], axis=0))
     # print(len(bucketed_samples), len(samples), bucketed_samples[0].shape, samples[0].shape)
-    return filterL2(bucketed_samples, sigma, expansion, itv)
+    return filterL2(bucketed_samples, eps, sigma, expansion, itv)
 
 def median(samples):
     return np.median(samples, axis=0)
